40ba7e3ebf9483867c99c9090b7eb697172db41e
zouxu <izouxv@gmail.com>
2016-05-02 14:32:05 +0800

    qiniu 7.0

---
+++ Target Support Files/Pods-qiniu/Pods-qiniu-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_qiniuVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_qiniuVersionString[];
+
---
+++ Target Support Files/Pods-qiniuUITests/Pods-qiniuUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qiniuUITests : NSObject
+@end
+@implementation PodsDummy_Pods_qiniuUITests
+@end
